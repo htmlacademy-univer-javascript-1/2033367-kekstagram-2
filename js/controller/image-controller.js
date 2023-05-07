ffee0966ada393b2a9cff8f75b54c6f29633676d
AndrewWhiteZ <ksarion1@gmail.com>
2023-05-07 12:40:13 +0500

    Added: Miniature Comments Pagination

--- js/controller/image-controller.js
+++ js/controller/image-controller.js
@@ -7,22 +7,14 @@ const commentsCount = bigPicture.querySelector('.comments-count');
 const commentsList = bigPicture.querySelector('.social__comments');
 const commentTemplate = commentsList.querySelector('.social__comment');
 const descriptionNode = bigPicture.querySelector('.social__caption');
-
+const showedCommentsCount = bigPicture.querySelector('.showed-comments-count');
 const closeButton = bigPicture.querySelector('.big-picture__cancel');
+let currentComments = undefined;
 
-const revealMiniature = function (url, likes, comments, description) {
-  bigPicture.classList.remove('hidden');
-  document.body.classList.add('modal-open');
-  document.querySelector('.social__comment-count').classList.add('hidden');
-  document.querySelector('.comments-loader').classList.add('hidden');
 
-  commentsList.innerHTML = '';
-  image.src = url;
-  likesCount.textContent = likes;
-  commentsCount.textContent = comments.length;
-  descriptionNode.textContent = description;
-
-  comments.forEach((comment) => {
+const revealComments = function(offset) {
+  let revealedCommentsCount = +showedCommentsCount.textContent;
+  currentComments.slice(revealedCommentsCount, revealedCommentsCount + offset).forEach(comment => { 
     const commentNode = commentTemplate.cloneNode(true);
     const commentImage = commentNode.querySelector('img');
     commentImage.src = comment.avatar;
@@ -30,14 +22,38 @@ const revealMiniature = function (url, likes, comments, description) {
     commentNode.querySelector('p').textContent = comment.message;
 
     commentsList.append(commentNode);
+    revealedCommentsCount++;
+  });
+
+  showedCommentsCount.textContent = revealedCommentsCount;
+  if (currentComments.length === revealedCommentsCount) {
+    document.querySelector('.comments-loader').classList.add('hidden');
+  } else {
+    document.querySelector('.comments-loader').classList.remove('hidden');
+  }
+}
+
+const revealMiniature = function(url, likes, comments, description) {
+  bigPicture.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+
+  commentsList.innerHTML = '';
+  image.src = url;
+  likesCount.textContent = likes;
+  commentsCount.textContent = comments.length;
+  descriptionNode.textContent = description;
+  showedCommentsCount.textContent = 0;
+  currentComments = comments;
+  revealComments(5);
+
+  document.querySelector('.comments-loader').addEventListener('click', () => {
+    revealComments(5);  
   });
 };
 
 const closeMiniature = function() {
   bigPicture.classList.add('hidden');
   document.body.classList.remove('modal-open');
-  document.querySelector('.social__comment-count').classList.remove('hidden');
-  document.querySelector('.comments-loader').classList.remove('hidden');
 };
 
 closeButton.addEventListener('click', closeMiniature);
